c24e6757690e4bfc8808f25fde135178e4fd2d84
murilo-silva-ai <murilo.vfs.472@gmail.com>
2025-03-26 11:57:16 -0300

    finalização dos testes principais

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -7,85 +7,199 @@ describe('Cypress simulator outputs', () => {
     })
     cy.contains('button', 'Login').click()
     cy.contains('h1', 'Cypress Simulator').should('be.visible')
-
   })
 
   context('it emulates cypress commands', () => {
-    it.only('passes', () => {
+    it('passes successfully', () => {
+      cy.get('#codeInput').type("cy.get('#body')")
+      cy.get('#runButton').click()
 
+      cy.get('#outputArea', { timeout: 6000 }).should('contain.text', "Success:\n\ncy.get('#body') // Got element by selector '#body'")
     })
 
     it('error: invalid cypress command', () => {
+      cy.get('#codeInput').type("cy.escolaTaT('Hello cypress')")
+      cy.get('#runButton').click()
 
+      cy.get('#outputArea', { timeout: 6000 }).should('contain.text', "Error:\n\nInvalid Cypress command: cy.escolaTaT('Hello cypress')")
     })
 
     it('error: valid command without parentheses', () => {
+      cy.get('#codeInput').type('cy.get')
+      cy.get('#runButton').click()
 
+      cy.get('#outputArea', { timeout: 6000 })
+        .should('contain.text', 'Error:')
+        .and('contain.text', 'Missing parentheses')
     })
 
-    it('output: warning', () => {
+    it('output: warning, command has not been implemented yet', () => {
+      cy.get('#codeInput').type("cy.contains('button', 'Run')")
+      cy.get('#runButton').click()
 
+      cy.get('#outputArea', { timeout: 6000 })
+        .should('contain.text', 'Warning:')
+        .and('contain.text', 'command has not been implemented yet.')
     })
 
-    it('help', () => {
+    it('asks for help and gets common Cypress commands and examples with a link to the docs', () => {
+      cy.get('#codeInput').type("help")
+      cy.get('#runButton').click()
 
+      cy.get('#outputArea', { timeout: 6000 })
+        .should('contain.text', 'Common Cypress commands and examples:')
+      cy.contains('#outputArea a', 'official Cypress API documentation')
+        .invoke('removeAttr', 'target')
+        .click()
+
+      cy.origin('https://docs.cypress.io', () => {
+        cy.contains('h1', 'Table of Contents')
+      })
     })
   })
 
   it('maximize/minimize output session', () => {
+    cy.get('.expand-collapse').click()
+    cy.get('.expand-collapse')
+      .invoke('attr', 'aria-expanded')
+      .should('eq', 'true')
 
+    cy.get('.expand-collapse').click()
+    cy.get('.expand-collapse')
+      .invoke('attr', 'aria-expanded')
+      .should('eq', 'false')
   })
 
-  it('logout', () => {
+  it('user logs out successfully', () => {
+    cy.get('button[aria-label="Open menu"]').click()
+    cy.contains('button', 'Logout')
+      .should('be.visible')
+      .click()
 
+    cy.contains('h2', "Let's get started!").should('be.visible')
   })
 
   it('show and hide logout button', () => {
+    cy.get('button[aria-label="Open menu"]').click()
 
+    cy.contains('button', 'Logout').should('be.visible')
+
+    cy.get('button[aria-label="Open menu"]').click()
+
+    cy.contains('button', 'Logout').should('not.be.visible')
   })
 
-  it('button run must not be clickable if no command is passed', () => {
+  it('verify run button clickability', () => {
+    cy.get('#codeInput').type('A')
 
+    cy.get('#runButton').should('be.enabled')
+
+    cy.get('#codeInput').clear()
+
+    cy.get('#runButton').should('not.be.enabled')
   })
 
-  it('button run must be clickable when command is passed', () => {
+  it('Assert the "Running... state" process', () => {
+    cy.get('#codeInput').type("cy.log('escolaTaT')")
+    cy.get('#runButton').click()
 
+    cy.get('#runButton').should('contain.text', 'Running...')
+    cy.get('#outputArea').should('contain.text', 'Running... Please wait.')
+
+    cy.contains('button', 'Running...', { timeout: 6000 }).should('not.exist')
+    cy.contains('#outputArea', 'Running... Please wait.', { timeout: 6000 }).should('not.exist')
+
+    cy.get('#outputArea', { timeout: 6000 }).should('contain.text', "cy.log('escolaTaT') // Logged message 'escolaTaT'")
   })
 
-  it('Running... state', () => {
+  it('Reset text area and code output on logout and login', () => {
+    cy.get('#codeInput').type("cy.log(message)")
+    cy.contains('button', 'Run').click()
+    cy.get('#outputArea', { timeout: 6000 })
+      .should('contain.text', 'Success:')
+      .and('contain.text', 'cy.log(message) // Logged message message')
+    cy.get('#sandwich-menu').click()
+    cy.get('#logoutButton').click()
+    cy.contains('button', 'Login').click()
 
-  })
-
-  it('Accept cookies', () => {
-
-  })
-
-  it('Decline cookies', () => {
-
-  })
-
-  it('Captcha button states', () => {
-
-  })
-
-  it('Captcha error', () => {
-
-  })
-
-  it('Reset text area on logout and login', () => {
-
+      cy.get('#codeInput').should('be.empty')
+      cy.get('#outputArea').should('be.empty')
   })
 
   it('Disable run button on logout and login', () => {
+    cy.get('#codeInput').type("cy.log(message)")
+    cy.contains('button', 'Run').should('be.enabled')
+    cy.get('#sandwich-menu').click()
+    cy.get('#logoutButton').click()
+    cy.contains('button', 'Login').click()
 
-  })
-
-  it('Clear output on logout and login', () => {
-
+    cy.contains('button', 'Run').should('be.disabled')
   })
 
   it('No cookie banner on login page', () => {
+    cy.clearAllLocalStorage()
+    cy.reload()
+    
+    cy.contains('button', 'Login').should('be.visible')
+    cy.get('#cookieConsent').should('not.be.visible')
+  })
+})
 
+describe('Cypress simulator - Cookies Consent', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html?skipCaptcha=true')
+    cy.contains('button', 'Login').click()
+    cy.contains('h1', 'Cypress Simulator').should('be.visible')
   })
 
+  it('Accept cookies', () => {
+    cy.get('#cookieConsent')
+      .as('cookieConsentBanner')
+      .find("button:contains('Accept')")
+      .click()
+
+    cy.get('@cookieConsentBanner').should('not.be.visible')
+    cy.window()
+      .its('localStorage.cookieConsent')
+      .should('be.equal', 'accepted')
+  })
+
+  it('Decline cookies', () => {
+    cy.get('#cookieConsent')
+      .as('cookieConsentBanner')
+      .find("button:contains('Decline')")
+      .click()
+
+    cy.get('@cookieConsentBanner').should('not.be.visible')
+    cy.window()
+      .its('localStorage.cookieConsent')
+      .should('be.equal', 'declined')
+  })
+})
+
+describe('Cypress simulator - Captcha and Login', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html?skipCaptcha=false')
+    cy.contains('button', 'Login').click()
+  })
+  it('Captcha button states', () => {
+    cy.contains('button', 'Verify').should('be.disabled')
+
+    cy.get('#captchaInput').type('123')
+
+    cy.contains('button', 'Verify').should('not.be.disabled')
+
+    cy.get('#captchaInput').clear()
+
+    cy.contains('button', 'Verify').should('be.disabled')
+  })
+
+  it('Captcha error', () => {
+    cy.get('#captchaInput').type('-123')
+    cy.contains('button', 'Verify').click()
+
+    cy.contains('p', 'Incorrect answer, please try again.').should('be.visible')
+    cy.get('#captchaInput').should('be.empty')
+    cy.contains('button', 'Verify').should('be.disabled')
+  })
 })
