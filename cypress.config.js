3e65870e1683df952655356df9c6b0c4a874cad7
murilo-silva-ai <murilo.vfs.472@gmail.com>
2025-03-27 10:55:23 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
+const cypressSplit = require("cypress-split")
+
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
   e2e: {
     // baseUrl: 'https://cypress-simulator.s3.eu-central-1.amazonaws.com/index.html',
     fixturesFolder: false,
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    }
   },
 })
